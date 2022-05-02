da3869dd2f4332817fc2c3e9a26f52b6bbee49e8
Charles Noah <91674419+NoahCrown@users.noreply.github.com>
2022-05-02 23:00:05 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+var playerScore = 0;
+var cpuScore = 0;
+
+for (var i = 0; i< document.querySelectorAll(".choice").length;i++){
+    document.querySelectorAll(".choice")[i].addEventListener("click", function(){
+        var playerChoice = this.value;
+        battleChoices(playerChoice, "player");
+        var cpuChoice = Math.floor((Math.random() * 3) + 1).toString();
+        battleChoices(cpuChoice, "cpu");
+
+        if (playerChoice === "1" && cpuChoice === "1"){
+            
+        }else if(playerChoice === "1" && cpuChoice ==="2"){
+            cpuScore++;
+            document.getElementById("cpuScore").innerHTML = cpuScore.toString();
+        }else if(playerChoice === "1" && cpuChoice ==="3"){
+            playerScore++;
+            document.getElementById("playerScore").innerHTML = playerScore.toString();
+        }else if(playerChoice ==="2" && cpuChoice==="1"){
+            playerScore++;
+            document.getElementById("playerScore").innerHTML = playerScore.toString();
+
+        }
+
+    } )
+}
+
+function battleChoices(choices, playerOrCpu){
+    switch(choices){
+        case "1":
+            document.getElementById(playerOrCpu).src="images/fist.png";
+            break;
+        case "2":
+            document.getElementById(playerOrCpu).src="images/palm.png";
+            break;
+        case "3":
+            document.getElementById(playerOrCpu).src="images/scissors.png";
+            break;
+        default:
+            alert("Error! 404!")
+    }
+}
+
